53076f98036e37ddc1f0624e6132c996a700ce40
Gergely Kiss <gergely.kiss@telrock.com>
2018-05-11 17:23:13 +0100

    initCommit

---
+++ src/main/java/com/mycompany/tensorflow_basic/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tensorflow_basic;
+
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+
+/**
+ *
+ * @author account
+ */
+public class Main {
+      public static void main(String[] args) throws Exception {
+    try (Graph g = new Graph()) {
+      final String value = "Hello from " + TensorFlow.version();
+
+      // Construct the computation graph with a single operation, a constant
+      // named "MyConst" with a value "value".
+      try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
+        // The Java API doesn't yet include convenience functions for adding operations.
+        g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
+      }
+
+      // Execute the "MyConst" operation in a Session.
+      try (Session s = new Session(g);
+           Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+        System.out.println(new String(output.bytesValue(), "UTF-8"));
+      }
+    }
+  }
+    
+}
